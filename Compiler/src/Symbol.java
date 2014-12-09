06f98979c5214b6e7c014ad7cf9514121ae08289
ahabthearab7 <cgduncan7@gmail.com>
2014-12-08 21:36:32 -0600

    Added symboltable

---
+++ Compiler/src/Symbol.java
@@ -0,0 +1,73 @@
+
+public class Symbol {
+	private String name;
+	private String block;
+	private int nesting;
+	private int offset;
+	private int nextUse;
+	private int locations;
+	
+	public Symbol(String n, String b, int ne, int o, int nu, int l) {
+		this.name = n;
+		this.block = b;
+		this.nesting = ne;
+		this.offset = o;
+		this.nextUse = nu;
+		this.locations = l;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name + ", " + this.block + ", " + this.nesting + ", " + this.offset + ", " + this.nextUse + ", " + this.locations;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBlock() {
+		return block;
+	}
+
+	public void setBlock(String block) {
+		this.block = block;
+	}
+
+	public int getNesting() {
+		return nesting;
+	}
+
+	public void setNesting(int nesting) {
+		this.nesting = nesting;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	public int getNextUse() {
+		return nextUse;
+	}
+
+	public void setNextUse(int nextUse) {
+		this.nextUse = nextUse;
+	}
+
+	public int getLocations() {
+		return locations;
+	}
+
+	public void setLocations(int locations) {
+		this.locations = locations;
+	}
+	
+	
+}
